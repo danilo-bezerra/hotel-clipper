853abcc8f886f4ac1d1812217ff20514e873e081
Danilo Bezerra <danilobezerra970@gmail.com>
2023-08-25 13:29:57 -0300

    add logo image

--- src/main/java/clipper/hotel/HelloApplication.java
+++ src/main/java/clipper/hotel/HelloApplication.java
@@ -6,10 +6,12 @@ import clipper.hotel.setup.DevSetup;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.Locale;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
@@ -19,11 +21,19 @@ public class HelloApplication extends Application {
 
         MainController.setPrimaryStage(stage);
 
+
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("auth-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Hotel Clipper");
+        stage.getIcons().add(new Image("https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png"));
         stage.setScene(scene);
+
         stage.show();
+
+
+
+
     }
 
     public static void main(String[] args) {
